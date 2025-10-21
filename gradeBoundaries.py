ace6b8778040739a286f58b76dd0c68353fa87c7
Annoying-Kiddd <annoying.kid08@hotmail.com>
2025-10-21 13:28:11 +0100

    Add files via upload

---
+++ gradeBoundaries.py
@@ -0,0 +1,14 @@
+mark = int(input('What is your mark?\n  '))
+
+if mark >= 56:
+    print('Grade A :D')
+elif mark >= 48:
+    print('grade B :)')
+elif mark >= 40:
+    print('grade C :)')
+elif mark >= 33:
+    print('grade D')
+elif mark >= 26:
+    print('grade E')
+else:
+    print('U :(')
